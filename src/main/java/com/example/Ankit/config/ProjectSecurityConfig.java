9569765cb1f99ca8c3e5eedd760de3f9ff6521ec
ankitvishwakarma91 <ankitvishwakarma9137@gmail.com>
2024-05-26 21:40:44 +0530

    updating contact file adding validation in contact form

---
+++ src/main/java/com/example/Ankit/config/ProjectSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.Ankit.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultsecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+
+
+    }
+}
