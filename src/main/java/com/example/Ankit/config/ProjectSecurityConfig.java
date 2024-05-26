df2ba7fb7beb0dfa8aeb18bf7f5cd7372cc9c3aa
ankitvishwakarma91 <ankitvishwakarma9137@gmail.com>
2024-05-26 21:47:51 +0530

    adding spring security

--- src/main/java/com/example/Ankit/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/Ankit/config/ProjectSecurityConfig.java
@@ -10,12 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectSecurityConfig {
 
+
+    // Permits all request
     @Bean
     SecurityFilterChain defaultsecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+
+
+    //Deny all permits
+//    @Bean
+//    SecurityFilterChain defaultsecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(request -> request.anyRequest().denyAll())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+//        return http.build();
 
 
     }
