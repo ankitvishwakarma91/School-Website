25c84cb7e450fb6acd68257b45295c1b7909cb4a
ankitvishwakarma91 <ankitvishwakarma9137@gmail.com>
2024-05-27 10:19:44 +0530

    adding spring security

--- src/main/java/com/example/Ankit/controller/LoginController.java
+++ src/main/java/com/example/Ankit/controller/LoginController.java
@@ -1,25 +1,29 @@
 package com.example.Ankit.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-public class loginController {
+@Slf4j
+@Controller
+public class LoginController {
 
-
-    @GetMapping("/login")
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
     public String displayLoginPage(@RequestParam(value = "error", required = false) String error,
                                    @RequestParam(value = "logout", required = false) String logout,
                                    Model model) {
-
         String errorMessge = null;
         if (error != null) {
-            errorMessge = "Invalid username or password";
+            errorMessge = "Username or Password is incorrect !!";
         }
         if (logout != null) {
-            errorMessge = "You have been logged out successfully";
+            errorMessge = "You have been successfully logged out !!";
         }
         model.addAttribute("errorMessge", errorMessge);
         return "login";
     }
+
 }
